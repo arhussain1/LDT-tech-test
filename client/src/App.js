4653519df04203a502b65eede037b626d9164c70
arhussain1 <a.r.hussain@hotmail.com>
2022-06-05 23:31:38 +0100

    added fetchBookings to retrieve bookings data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { useEffect } from 'react'
+
 
 function App() {
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      await fetch("http://localhost:8001/data")
+      .then(response => response.json())
+      .then(data => console.log(data))
+      }
+    
+    fetchBookings();
+  }, []);
+
   return (
     <div className="App">
       Lettuce do this
