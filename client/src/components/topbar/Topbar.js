77a2ea86139615a60f446c8439c9698dee06f760
arhussain1 <a.r.hussain@hotmail.com>
2022-06-06 00:07:38 +0100

    created the Topbar with a logo

---
+++ client/src/components/topbar/Topbar.js
@@ -0,0 +1,13 @@
+import "./topbar.css"
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="logo">
+        Let us do this
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
